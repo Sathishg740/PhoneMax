fca0cd72778bfda2ef9566d9417317ac25647a4c
Sathish G <sathish740g@gmail.com>
2021-12-07 18:30:35 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneMax_1._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserDetails()
+        {
+            return View();
+        }
+        public IActionResult LoginSignupPage()
+        {
+            return View();
+        }
+        public IActionResult Android()
+        {
+            return View();
+        }
+        public IActionResult Galaxy_M52()
+        {
+            return View();
+        }
+        public IActionResult GalaxyZ()
+        {
+            return View();
+        }
+        public IActionResult GalaxyNote()
+        {
+            return View();
+        }
+        public IActionResult GalaxyS21()
+        {
+            return View();
+        }
+        public IActionResult Redmi10Prime()
+        {
+            return View();
+        }
+        public IActionResult Redmi9()
+        {
+            return View();
+        }
+        public IActionResult RedmiNote10S()
+        {
+            return View();
+        }
+        public IActionResult Redmi9Activ()
+        {
+            return View();
+        }
+        public IActionResult A74()
+        {
+            return View();
+        }
+        public IActionResult A31()
+        {
+            return View();
+        }
+        public IActionResult A16()
+        {
+            return View();
+        }
+        public IActionResult F19()
+        {
+            return View();
+        }
+        public IActionResult Iphone()
+        {
+            return View();
+        }
+        public IActionResult Iphone5()
+        {
+            return View();
+        }
+        public IActionResult Iphone6()
+        {
+            return View();
+        }
+        public IActionResult Iphone7()
+        {
+            return View();
+        }
+        public IActionResult Iphone8()
+        {
+            return View();
+        }
+        public IActionResult Iphone11Pro()
+        {
+            return View();
+        }
+        public IActionResult Iphone11ProMax()
+        {
+            return View();
+        }
+        public IActionResult Iphone12Pro()
+        {
+            return View();
+        }
+        public IActionResult Iphone13ProMax()
+        {
+            return View();
+        }
+        public IActionResult Windows()
+        {
+            return View();
+        }
+        public IActionResult Sucess()
+        {
+            return View();
+        }
+    }
+}
